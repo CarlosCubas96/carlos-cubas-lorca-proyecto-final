4c097f9e04b55b25ec913819c5ee7e651181afb6
CarlosCubas96 <prog.carlocubas@gmail.com>
2024-04-04 10:19:54 +0200

    Commit | Inicio de proyecto

---
+++ src-api-bicirent/BiciRentAPI/src/main/java/com/proyect/api/bicirent/BiciRentApiApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.api.bicirent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiciRentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiciRentApiApplication.class, args);
+	}
+
+}
---
+++ src-api-bicirent/BiciRentAPI/src/test/java/com/proyect/api/bicirent/BiciRentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.api.bicirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiciRentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
